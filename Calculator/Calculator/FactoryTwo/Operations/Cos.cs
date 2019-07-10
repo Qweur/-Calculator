1754372d80d0b86dc939af8df467328767558375
Qweur <trollingvolf23@gmail.com>
2019-07-10 23:17:50 +0600

    CosTests

--- Calculator/Calculator/FactoryTwo/Operations/Cos.cs
+++ Calculator/Calculator/FactoryTwo/Operations/Cos.cs
@@ -2,11 +2,12 @@
 
 namespace Calculator.FactoryTwo.Operations
 {
-    class Cos : IOneArgumentsCalculator
+   public class Cos : IOneArgumentsCalculator
     {
     public double CalculateTwo(double first)
     {
         return Math.Cos(first);
     }
+
     }
 }
