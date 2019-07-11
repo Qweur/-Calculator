0267442933d15b06cffb387dc435bcc0e130e639
Reechat <stasgnetul94@gmail.com>
2019-07-11 14:08:10 +0600

    Add Error message

--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -21,22 +21,34 @@ namespace Calculator
 
         private void Calculate_Two(object sender, EventArgs e)
         {
-            double firstOperand = Convert.ToDouble(Input1.Text);
-            IOneArgumentsCalculator calculator = FactoryTwo.CreateCalculatorTwo(((Button)sender).Name);
-            double result = calculator.CalculateTwo(firstOperand);
+            try
+            {
+                double firstOperand = Convert.ToDouble(Input1.Text);
+                IOneArgumentsCalculator calculator = FactoryTwo.CreateCalculatorTwo(((Button) sender).Name);
+                double result = calculator.CalculateTwo(firstOperand);
 
-
-            Result.Text = result.ToString(CultureInfo.InvariantCulture);
+                Result.Text = result.ToString(CultureInfo.InvariantCulture);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
         }
         private void Calculate(object sender, EventArgs e)
         {
-            double firstOperand = Convert.ToDouble(Input1.Text);
-            double secondOperand = Convert.ToDouble(Input2.Text);
-            ITwoArgumentsCalculator calculator = Factory.CreateCalculator(((Button)sender).Name);
-            double result = calculator.Calculate(firstOperand,secondOperand);
+            try
+            {
+                double firstOperand = Convert.ToDouble(Input1.Text);
+                double secondOperand = Convert.ToDouble(Input2.Text);
+                ITwoArgumentsCalculator calculator = Factory.CreateCalculator(((Button) sender).Name);
+                double result = calculator.Calculate(firstOperand, secondOperand);
+                Result.Text = result.ToString(CultureInfo.InvariantCulture);
 
-
-            Result.Text = result.ToString(CultureInfo.InvariantCulture);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
 
         }
     }
