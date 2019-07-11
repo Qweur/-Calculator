699a0ad9cec232ee3a89ded0f25f6d7f1dc18d13
Reechat <stasgnetul94@gmail.com>
2019-07-11 14:24:26 +0600

    First XML-message

--- Calculator/Calculator/OneArgumentsOperations/Arcsin.cs
+++ Calculator/Calculator/OneArgumentsOperations/Arcsin.cs
@@ -2,8 +2,20 @@
 
 namespace Calculator.OneArgumentsOperations
 {
+    /// <summary>
+    /// Count Arcsin
+    /// </summary>
     public class Arcsin : IOneArgumentsCalculator
     {
+        /// <summary>
+        /// Finding arcsin
+        /// </summary>
+        /// <param name="first">
+        /// Value arcsin
+        /// </param>
+        /// <returns>
+        /// Arcsin results
+        /// </returns>
         public double CalculateTwo(double first)
         {
             if (first > 1 || first < -1)
